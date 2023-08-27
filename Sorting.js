172ab1278666747694d78ee46b676bef47a9ced1
Darren Woo <103203561+DarrenWoo08@users.noreply.github.com>
2023-08-27 23:22:14 +0800

    Add files via upload

---
+++ Sorting.js
@@ -0,0 +1,43 @@
+import {
+    state,
+    sortingBtnRecentEl,
+    sortingEl,
+    sortingBtnRelevantEl
+
+}from '../common.js';
+
+import renderJobList from './JobList.js';
+import renderPaginationButton from './Pagination.js';
+
+const clickHandler = event => {
+    const clickedButtonEl = event.target.closest('.sorting__button');
+    if(!clickedButtonEl) return;
+    
+    state.currentPage = 1;
+
+    const recent = clickedButtonEl.className.includes('--recent') ? true : false;
+
+    if(recent) {
+        sortingBtnRecentEl.classList.add('sorting__button--active');
+        sortingBtnRelevantEl.classList.remove('sorting__button--active');
+    } else {
+        sortingBtnRecentEl.classList.remove('sorting__button--active');
+        sortingBtnRelevantEl.classList.add('sorting__button--active');
+    }
+
+    if (recent) {
+        state.searchJobItems.sort((a,b) => {
+            return a.daysAgo - b.daysAgo;
+        });
+    } else {
+        state.searchJobItems.sort((a,b) => {
+            return b.relevanceScore - a.relevanceScore;
+        });
+    }
+
+    renderPaginationButton();
+    
+    renderJobList();
+}
+
+sortingEl.addEventListener('click', clickHandler);
